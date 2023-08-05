daa74df8e9d9903c2e16ed58a7a6473c5ab831b7
GlebAQA <galievgleb99@gmail.com>
2023-08-05 13:47:32 +0300

    add oop. Visible and base_class create

--- pages/src/visible.py
+++ pages/src/visible.py
@@ -1,42 +1,24 @@
-import os
 from playwright.sync_api import Playwright, sync_playwright
+from base.base_class import BasePage
 
-screenshot_counter = 0
 
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(
-        viewport={"width": 1280, "height": 720}
-    )
+    context = browser.new_context(viewport={"width": 1280, "height": 720})
     page = context.new_page()
     page.goto("http://localhost:5173/#/visuallyImpaired")
 
-    click_with_screenshot(page, page.locator("label").nth(1))
-    click_with_screenshot(page, page.get_by_title("Большой"))
-    click_with_screenshot(page, page.get_by_role("button", name="A A"))
-    click_with_screenshot(page, page.get_by_role("button", name="large"))
-    click_with_screenshot(page, page.get_by_title("Черный на белом"))
-    click_with_screenshot(page, page.get_by_title("Желтый на черном"))
+    base_page = BasePage(page)
+
+    base_page.click_with_screenshot(page.locator("label").nth(1))
+    base_page.click_with_screenshot(page.get_by_title("Большой"))
+    base_page.click_with_screenshot(page.get_by_role("button", name="A A"))
+    base_page.click_with_screenshot(page.get_by_role("button", name="large"))
+    base_page.click_with_screenshot(page.get_by_title("Черный на белом"))
+    base_page.click_with_screenshot(page.get_by_title("Желтый на черном"))
 
     context.close()
     browser.close()
-
-
-def take_screenshot(page, screenshot_name):
-    project_root = os.getcwd()
-    screenshots_dir = os.path.join(project_root, "screenshots_test")
-    os.makedirs(screenshots_dir, exist_ok=True)
-    screenshot_path = os.path.join(screenshots_dir, f"screenshot_{screenshot_name}.png")
-    page.screenshot(path=screenshot_path)
-
-
-def click_with_screenshot(page, element):
-    global screenshot_counter
-    screenshot_counter += 1
-
-    element.click()
-    page.wait_for_timeout(500)  # Ожидаем 500 мс (0.5 секунды)
-    take_screenshot(page, screenshot_counter)  # Сделать скриншот после клика на элементе
 
 
 with sync_playwright() as playwright:
